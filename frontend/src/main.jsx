e12f267f66d2f1d845e3228005cd4281ab40155d
nikhil chhetri <nikhil142004@gmail.com>
2026-03-25 02:31:54 +0530

    fix: remove broken initializeTheme import from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { initializeTheme } from "./store/uiStore";
-
-initializeTheme();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
